1fa6cfea3ed0e6d04a29b29a535d9c3bddb99e50
Oskar Ehnstrom <oskar.e@gmail.com>
2018-09-10 16:29:43 +0000

    Add scripts

---
+++ ruuvi_rx.py
@@ -0,0 +1,28 @@
+from ruuvitag_sensor.ruuvi_rx import RuuviTagReactive
+import requests
+from datetime import datetime
+
+
+host = '10.0.0.20'
+
+params = {'db': 'temperatures'}
+
+def send_data(data):
+  mac = data[0]
+  items = data[1]
+  temperature = items['temperature']
+  humidity = items['humidity']
+  pressure = items['pressure']
+  data='ruuvi,address=Kilonportti,mac={} temperature={},humidity={},pressure={}'.format(mac, temperature, humidity, pressure)
+  r = requests.post('http://{}:8086/write'.format(host), params=params, data=data)
+  print('{} {} - {}'.format(datetime.now(), r.status_code, mac))
+  #print('{}: {}'.format(mac, temperature))
+
+
+print('Starting RuuviChip')
+
+ruuvi_rx = RuuviTagReactive()
+
+ruuvi_rx.get_subject().\
+  group_by(lambda x: x[0]).\
+  subscribe(lambda x: x.sample(15000).subscribe(send_data))
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+/usr/bin/python3 /root/ruuvi_rx.py > /var/log/ruuvi.log
